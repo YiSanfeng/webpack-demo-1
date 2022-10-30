28063cd5ea62b53f0094771bcef2eb2611ea7cdb
yisanfeng <1835397949@qq.com>
2022-10-31 00:01:33 +0800

    运行 yarn build 和 yarn start 报错

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,44 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
   mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+  },
   entry: "./src/index.js",
   output: {
-    filename: "[name].[contenthash].js",
+    filename: "index.[contenthash].js",
   },
-  // plugins: [new HtmlWebpackPlugin()],
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "XDML - 写代码啦",
+      template: "src/assets/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+      chunkFilename: "[id].[contenthash].css",
+      ignoreOrder: false,
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: "../",
+              hmr: process.env.NODE_ENV === "development",
+            },
+          },
+          "css-loader",
+        ],
+        // use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
