a76b616f7dc69ed169deb5d85b6853db7c45bb71
yisanfeng <1835397949@qq.com>
2022-10-30 22:14:09 +0800

    init

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
@@ -6,4 +7,5 @@ module.exports = {
   output: {
     filename: "[name].[contenthash].js",
   },
+  // plugins: [new HtmlWebpackPlugin()],
 };
