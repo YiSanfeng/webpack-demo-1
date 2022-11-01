ccdea09169b4893dc0fb3499c15ac4047ac82bed
yisanfeng <1835397949@qq.com>
2022-11-01 20:10:22 +0800

    yarn start报错

---
+++ webpack.config.base.js
@@ -0,0 +1,17 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    filename: "index.[contenthash].js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "XDML - 写代码啦",
+      template: "src/assets/index.html",
+    }),
+  ],
+};
--- webpack.config.js
+++ webpack.config.js
@@ -2,42 +2,21 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
+const base = require("./webpack.config.base.js");
+
 module.exports = {
-  mode: "development",
+  ...base,
   devtool: "inline-source-map",
   devServer: {
-    contentBase: "./dist",
+    static: {
+      directory: "./dist",
+    },
   },
-  entry: "./src/index.js",
-  output: {
-    filename: "index.[contenthash].js",
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "XDML - 写代码啦",
-      template: "src/assets/index.html",
-    }),
-    new MiniCssExtractPlugin({
-      filename: "[name].[contenthash].css",
-      chunkFilename: "[id].[contenthash].css",
-      ignoreOrder: false,
-    }),
-  ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              publicPath: "../",
-              hmr: process.env.NODE_ENV === "development",
-            },
-          },
-          "css-loader",
-        ],
-        // use: ["style-loader", "css-loader"],
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
